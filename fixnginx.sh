036cf98775d3ca1cfc28dc7fa86f61a38468d8ac
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-03-10 17:03:29 +0700

    Update fixnginx.sh

--- fixnginx.sh
+++ fixnginx.sh
@@ -5,4 +5,4 @@ rm -f /etc/nginx/conf.d/nginx.conf >/dev/null 2>&1
 wget https://github.com/zhets/ScriptAutoInstall-xdxl/raw/main/xdproject.conf -O /etc/nginx/conf.d/xdproject.conf
 wget https://github.com/zhets/ScriptAutoInstall-xdxl/raw/main/nginx.conf -O /etc/nginx/nginx.conf
 echo -e "\033[0;35m Successfully Fix Nginx & Change Port Nginx 81 > 445 \033[0m"
-sleep 3;menu
+sleep 2;menu
