fe7bb1178b1c9b334fd128fb848e7156a0f76b92
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-03-15 18:14:39 +0700

    Update fixnginx.sh

--- fixnginx.sh
+++ fixnginx.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 rm -f /etc/nginx/nginx.conf
-rm -f /etc/nginx/conf.d/xdproject.conf
-rm -f /etc/nginx/conf.d/nginx.conf >/dev/null 2>&1
-wget https://github.com/zhets/ScriptAutoInstall-xdxl/raw/main/xdproject.conf -O /etc/nginx/conf.d/xdproject.conf
+rm -f /etc/nginx/conf.d/xray.conf
+rm -f /etc/haproxy/haproxy.cfg >/dev/null 2>&1
+wget https://github.com/zhets/ScriptAutoInstall-xdxl/raw/main/xray.conf -O /etc/nginx/conf.d/xray.conf
 wget https://github.com/zhets/ScriptAutoInstall-xdxl/raw/main/nginx.conf -O /etc/nginx/nginx.conf
-echo -e "\033[0;35m Successfully Fix Nginx & Change Port Nginx 81 > 445 \033[0m"
+wget https://github.com/zhets/ScriptAutoInstall-xdxl/raw/main/haproxy.cfg -O /etc/haproxy/haproxy.cfg
+cat /etc/xray/xray.crt /etc/xray/xray.key | tee /etc/haproxy/xdxl.pem
+systemctl restart nginx haproxy ws
+echo -e "\033[0;35m Successfully Fix Nginx, Haproxy, Ws Epro Off \033[0m"
 sleep 2;menu
