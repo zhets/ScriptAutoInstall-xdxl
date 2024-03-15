f6fe9332fc57924d528e8f231b46b3a68db69d03
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-03-15 18:16:49 +0700

    Update fixnginx.sh

--- fixnginx.sh
+++ fixnginx.sh
@@ -5,7 +5,11 @@ rm -f /etc/haproxy/haproxy.cfg >/dev/null 2>&1
 wget https://github.com/zhets/ScriptAutoInstall-xdxl/raw/main/xray.conf -O /etc/nginx/conf.d/xray.conf
 wget https://github.com/zhets/ScriptAutoInstall-xdxl/raw/main/nginx.conf -O /etc/nginx/nginx.conf
 wget https://github.com/zhets/ScriptAutoInstall-xdxl/raw/main/haproxy.cfg -O /etc/haproxy/haproxy.cfg
+wget https://github.com/zhets/ScriptAutoInstall-xdxl/raw/main/tun.conf -O /usr/bin/tun.conf
 cat /etc/xray/xray.crt /etc/xray/xray.key | tee /etc/haproxy/xdxl.pem
+domain=$(cat /etc/xray/domain)
+sed -i "s/xxx/${domain}/g" /etc/haproxy/haproxy.cfg
+sed -i "s/xxx/${domain}/g" /etc/nginx/conf.d/xray.conf
 systemctl restart nginx haproxy ws
 echo -e "\033[0;35m Successfully Fix Nginx, Haproxy, Ws Epro Off \033[0m"
 sleep 2;menu
