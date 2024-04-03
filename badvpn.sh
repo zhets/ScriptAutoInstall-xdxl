b15333aacba8971e433c5d116b8f56076e1ab387
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-04-04 06:47:26 +0700

    Update badvpn.sh

--- badvpn.sh
+++ badvpn.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 REPO="https://raw.githubusercontent.com/zhets/zhets/ScriptAutoInstall-xdxl/main/"
 mkdir -p /usr/local/xdxl/
-wget -q -O /usr/local/xdxl/badvpn-udpgpw "${REPO}badvpn/badvpn-udpgpw"
-chmod +x /usr/local/xdxl/badvpn-udpgpw
-wget -q -O /etc/systemd/system/badvpn-1.service "${REPO}badvpn/badvpn-1.service"
-wget -q -O /etc/systemd/system/badvpn-2.service "${REPO}badvpn/badvpn-2.service"
-wget -q -O /etc/systemd/system/badvpn-3.service "${REPO}badvpn/badvpn-3.service"
-wget -q -O /etc/systemd/system/badvpn-4.service "${REPO}badvpn/badvpn-4.service"
-wget -q -O /etc/systemd/system/badvpn-5.service "${REPO}badvpn/badvpn-5.service"
+wget -q -O /usr/local/xdxl/badvpn "${REPO}badvpn/badvpn"
+chmod +x /usr/local/xdxl/badvpn
+wget -q -O /etc/systemd/system/badvpn-1.service "${REPO}badvpn/badvpn1.service"
+wget -q -O /etc/systemd/system/badvpn-2.service "${REPO}badvpn/badvpn2.service"
+wget -q -O /etc/systemd/system/badvpn-3.service "${REPO}badvpn/badvpn3.service"
+wget -q -O /etc/systemd/system/badvpn-4.service "${REPO}badvpn/badvpn4.service"
+wget -q -O /etc/systemd/system/badvpn-5.service "${REPO}badvpn/badvpn5.service"
 
 systemctl disable badvpn-1
 systemctl stop badvpn-1
